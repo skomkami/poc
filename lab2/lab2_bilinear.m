2feb6db1a1013a3bccd106b2aa61724e89815d1a
kamil <kamilskomrokamil@gmail.com>
2019-10-09 20:34:03 +0200

    repair bilinear method compile error

--- lab2/lab2_bilinear.m
+++ lab2/lab2_bilinear.m
@@ -20,22 +20,23 @@ Img2 = double(Img);
 
 for ii  = 1 : nYY
     i = ii*xStep;
-    
-    for jj = 1 : nXX
-        j = jj*yStep;
-        A = floor([round(j), round(i)]);
-        B = A + [1, 0];
-        C = A + [1, 1];
-        D = A + [0, 1];
-        fA = Img2(A);
-        fB = Img2(B);
-        fC = Img2(C);
-        fD = Img2(D);
-        current = (A - [i,j]);
-        temp = [1-current(1) current(1)]*[fA fD; fB fC];
-        nI(ii,jj) = temp .* [1-current(2); current(2)];    
-    end
-end
-
-figure(2);
-img2 = imshow(uint8(nI));
+     for jj = 1 : nXX
+         j = jj*yStep;
+         A = floor([i, j]);
+         B = A + [1, 0];
+         C = A + [1, 1];
+         D = A + [0, 1];
+         if floor(C(1) + C(2)*XX)+1 > 10000 || floor(D(1) + D(2)*XX)+1 > 10000
+            break
+         end
+         fA = Img2(floor(A(1)+A(2)*XX)+1);
+         fB = Img2(floor(B(1)+B(2)*XX)+1);
+         fC = Img2(floor(C(1)+C(2)*XX)+1);
+         fD = Img2(floor(D(1)+D(2)*XX)+1);
+         current = [i,j]-A;
+         nI(ii,jj) = [(1-current(1)) current(1)]*[fA fD; fB fC]*[(1-current(2)); current(2)];
+     end
+ end
+ 
+ figure(2);
+ img2 = imshow(uint8(nI));
