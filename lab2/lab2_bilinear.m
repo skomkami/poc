8c500e2143946f2d4989659b7afc4a3b5d3d7b15
kamil <kamilskomrokamil@gmail.com>
2019-10-12 17:51:57 +0200

    repair bilinear interpolation

--- lab2/lab2_bilinear.m
+++ lab2/lab2_bilinear.m
@@ -4,8 +4,8 @@ figure(1);
 Img = imread("parrot.bmp");
 img1 = imshow(Img);
 
-xReScale = 1.5;
-yReScale = 1.5;
+xReScale = 2;
+yReScale = 2;
 [YY, XX] = size(Img);
 
 nYY = round(YY * yReScale);
@@ -22,20 +22,23 @@ for jj = 0 : nYY-1
     j = jj*yStep;
      for ii = 0 : nXX-1
          i = ii*xStep;
-         A = floor([i, j]);
+         A = floor([j,i]);
          B = A + [1, 0];
          C = A + [1, 1];
          D = A + [0, 1];
-         if ii == nXX-1 || jj == nYY-1
-            nI(ii+1,jj+1) = Img2(A(1)+1,A(2)+1);
+         if i >= XX-1 || j >= YY-1
+            nI(jj+1,ii+1) = Img2(A(1)+1,A(2)+1);
             continue
          end
          fA = Img2(A(1)+1,A(2)+1);
          fB = Img2(B(1)+1,B(2)+1);
          fC = Img2(C(1)+1,C(2)+1);
          fD = Img2(D(1)+1,D(2)+1);
-         current = [i,j]-A;
-         nI(ii+1,jj+1) = [(1-current(1)) current(1)]*[fA fD; fB fC]*[(1-current(2)); current(2)];
+         current = [j,i]-A;
+         y = current(1);
+         x = current(2);
+         
+         nI(jj+1,ii+1) = [(1-x) x]*[fA fD; fB fC]*[(1-y); y];
      end
  end
  
