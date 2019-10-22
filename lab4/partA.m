cd0af67cf0338961ea11398293eb5658288c392f
Kamil Skomro <kamil.skomr97@gmail.com>
2019-10-22 18:20:00 +0200

    add lab4

--- lab4/partA.m
+++ lab4/partA.m
@@ -40,3 +40,83 @@ imshow(adjhist);
 subplot(1,2,2);
 imhist(adjhist, 256);
 
+%% histogram skumulowany
+
+[H,x] = imhist(hist1,256);
+C = cumsum(H);
+
+figure(4);
+k = max(C)/max(H);
+C2 = C/k;
+plot(x,H);
+hold on;
+plot(x,C2);
+hold off;
+legend('histogram','histogram skumulowany');
+
+% lut
+lut = C2*255/max(C2);
+lut = uint8(lut);
+histlut = intlut(hist1, lut);
+figure(5);
+subplot(3,1,1);
+imshow(histlut);
+subplot(3,1,2);
+plot(x,H);
+subplot(3,1,3);
+plot(x,C2);
+
+%% 13
+
+X = histeq(hist1, 256);
+
+figure(6);
+subplot(3,1,1);
+imshow(X);
+subplot(3,1,2);
+imhist(X,256);
+subplot(3,1,3);
+J = adapthisteq(hist1);
+imhist(J);
+
+%14
+
+hist2 = imread('hist2.bmp');
+hist2adj = imadjust(hist2);
+hist3 = imread('hist3.bmp');
+hist3adj = imadjust(hist3);
+hist4 = imread('hist4.bmp');
+hist4adj = imadjust(hist4);
+
+figure(7);
+subplot(2,2,1);
+imshow(hist2);
+subplot(2,2,2);
+imshow(hist2adj);
+subplot(2,2,3);
+histeq(hist2, 256);
+subplot(2,2,4);
+J = adapthisteq(hist2);
+imshow(J);
+
+figure(8);
+subplot(2,2,1);
+imshow(hist3);
+subplot(2,2,2);
+imshow(hist3adj);
+subplot(2,2,3);
+histeq(hist3, 256);
+subplot(2,2,4);
+J = adapthisteq(hist3);
+imshow(J);
+
+figure(9);
+subplot(2,2,1);
+imshow(hist4);
+subplot(2,2,2);
+imshow(hist4adj);
+subplot(2,2,3);
+histeq(hist4, 256);
+subplot(2,2,4);
+J = adapthisteq(hist4);
+imshow(J);
